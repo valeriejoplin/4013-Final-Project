ec774bb5cb738ca69414c8f430f1e57500c972ad
Andrew Soltis <ams.soltis@ou.edu>
2022-11-24 11:14:05 -0600

    test

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -19,9 +19,9 @@
       <th>Quantity</th>
     </tr>
                                                         
-    
+    <tr>
      <?php for($i = 0 ; $i < count($_SESSION['cart']) ; $i++) {
-     echo '<tr><td>'.$_SESSION['cart'][$i].'</td';
+     echo '$_SESSION['cart'][$i].';
      }  ?>
     </tr>
 </table>
