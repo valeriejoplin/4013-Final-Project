dcd68b6750fad9d287228713ed65e16263821b6e
Andrew Soltis <ams.soltis@ou.edu>
2022-11-24 10:11:01 -0600

    test

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -1,8 +1,8 @@
         <?php
         session_start();
-        if(isset($_SESSION['cart'])){
-        return $_SESSION['cart'] = array();
-        }
+		
+  
+        $_SESSION['cart'] = array();
 		
         array_push($_SESSION['cart'], $_GET['product_id']);
         ?>
