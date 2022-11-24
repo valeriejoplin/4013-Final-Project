c7ce7c1f7c16abe5aa6e3c2415e549886bad51de
Andrew Soltis <ams.soltis@ou.edu>
2022-11-24 09:32:51 -0600

    test

--- frontEndCart.php
+++ frontEndCart.php
@@ -10,9 +10,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$iid = $_POST['product_id'];
+$product = $_POST['product_id'];
+$quantity = $_POST['quantity'];
+
 //echo $iid;
-$sql = "Select * From product Where productID =".$iid;
+$sql = "Select * From product Where productID =".$product;
 //echo $sql;
     $result = $conn->query($sql);
 
@@ -20,6 +22,21 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
+if ($product && $quantity > 0) {
+        // Product exists in database, now we can create/update the session variable for the cart
+        if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
+            if (array_key_exists($product_id, $_SESSION['cart'])) {
+                // Product exists in cart so just update the quanity
+                $_SESSION['cart'][$product_id] += $quantity;
+            } else {
+                // Product is not in cart so add it
+                $_SESSION['cart'][$product_id] = $quantity;
+            }
+        } else {
+            // There are no products in cart, this will add the first product to cart
+            $_SESSION['cart'] = array($product_id => $quantity);
+        }
+    }
                 <h1><?=$row["name"]?></h1>
 
  <?php
