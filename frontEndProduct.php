ce2e9777a072691db05c586f8b05e93e56e24525
Andrew Soltis <ams.soltis@ou.edu>
2022-11-24 09:04:16 -0600

    test

--- frontEndProduct.php
+++ frontEndProduct.php
@@ -170,7 +170,7 @@ if ($result->num_rows > 0) {
                                 
                 <p>Qty Avalible: <?=$row["qtyAvalible"]?></p>
                         <button type="button">Buy Now</button>
-            <form action="./frontEndCart.php" method="post">
+            <form action="./frontEndCart.php" method="get">
             <input type="number" name="quantity" value="1" min="1" max="<?=$row["qtyAvalible"]?>" placeholder="Quantity" required>
             <input type="hidden" name="product_id" value="">
             <input type="submit" value="Add To Cart">
