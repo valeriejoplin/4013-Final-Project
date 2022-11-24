4ad648ce62072dd005d118212592d8985417f805
Andrew Soltis <ams.soltis@ou.edu>
2022-11-24 09:40:21 -0600

    t

--- frontEndCart.php
+++ frontEndCart.php
@@ -1,46 +1,45 @@
- <?php
+ <?php 
                         $servername = "localhost";
                         $username = "asoltiso_project";
                         $password = "Project1243";
-                        $dbname = "asoltiso_project";   
+                        $dbname = "asoltiso_project";  
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-$product = $_POST['product_id'];
-$quantity = $_POST['quantity'];
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    $product = $_POST['product_id'];
+    $quantity = $_POST['quantity'];
 
-//echo $iid;
-$sql = "Select * From product Where productID =".$product;
-//echo $sql;
-    $result = $conn->query($sql);
+    //echo $iid;
+    $sql = "Select * From product Where productID =".$product;
+    //echo $sql;
+        $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-      if ($product && $quantity > 0) {
-        // Product exists in database, now we can create/update the session variable for the cart
-        if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
-            if (array_key_exists($product_id, $_SESSION['cart'])) {
-                // Product exists in cart so just update the quanity
-                $_SESSION['cart'][$product_id] += $quantity;
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          if ($product && $quantity > 0) {
+            // Product exists in database, now we can create/update the session variable for the cart
+            if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
+                if (array_key_exists($product_id, $_SESSION['cart'])) {
+                    // Product exists in cart so just update the quanity
+                    $_SESSION['cart'][$product_id] += $quantity;
+                } else {
+                    // Product is not in cart so add it
+                    $_SESSION['cart'][$product_id] = $quantity;
+                }
             } else {
-                // Product is not in cart so add it
-                $_SESSION['cart'][$product_id] = $quantity;
+                // There are no products in cart, this will add the first product to cart
+                $_SESSION['cart'] = array($product_id => $quantity);
             }
-        } else {
-            // There are no products in cart, this will add the first product to cart
-            $_SESSION['cart'] = array($product_id => $quantity);
+           }        
         }
-    }        
-  }
-}
-
+    }
  ?>
-
+                         
 <?php
   }
 } else {
