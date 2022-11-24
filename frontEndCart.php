1ed4e670ae579b96d671ba479f5339a234ea401b
Andrew Soltis <ams.soltis@ou.edu>
2022-11-24 09:16:49 -0600

    test

--- frontEndCart.php
+++ frontEndCart.php
@@ -1,3 +1,30 @@
-$id=$_GET['product_id'];
+<?php
+                        $servername = "localhost";
+                        $username = "asoltiso_project";
+                        $password = "Project1243";
+                        $dbname = "asoltiso_project";   
 
-<p>$id</p>
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$id = $_GET['product_id'];
+//echo $iid;
+$sql = "Select * From product Where productID =".$id;
+//echo $sql;
+    $result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+                <h1><?=$row["name"]?></h1>
+                        <?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
