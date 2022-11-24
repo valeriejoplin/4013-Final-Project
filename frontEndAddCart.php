356f9f18d94b00c44b5f6924ae69a5c34cc042ff
Andrew Soltis <ams.soltis@ou.edu>
2022-11-24 10:42:17 -0600

    test

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -7,18 +7,42 @@
     }
     $product=$_POST['product_id']; //student_name form field name
     array_push($_SESSION['cart'],$product);   
-    print_r($_SESSION['cart']);
+    //print_r($_SESSION['cart']);
 ?>
 
-<table class="table">     <!-- HTML Part (optional) -->
-    <tr>
-      <th>Product ID</th>
-    </tr>
-                                                        
-    
-     <?php for($i = 0 ; $i < count($_SESSION['cart']) ; $i++) {
-     echo '<tr><td>'.$_SESSION['cart'][$i].'</td>
-</tr>';
-     }  ?>
-    
-</table>
+<?php
+                        $servername = "localhost";
+                        $username = "asoltiso_project";
+                        $password = "Project1243";
+                        $dbname = "asoltiso_project";   
+						
+
+                            // Create connection
+                            $conn = new mysqli($servername, $username, $password, $dbname);
+                            // Check connection
+                            if ($conn->connect_error) {
+                              die("Connection failed: " . $conn->connect_error);
+                            }
+							
+                            $sql = "SELECT * FROM product Where productID =".$product;
+                            //echo $sql;
+                            $result = $conn->query($sql);
+
+                            if ($result->num_rows > 0) {
+                              // output data of each row
+                              while($row = $result->fetch_assoc()) {
+                            ?>
+                              <div class=product>
+				      <a href="./frontEndProduct.php?id=<?=$row["productID"]?>">
+                        <img src="assets/<?=$row["img"]?>.png" />
+                    </a>
+				      <h1><?=$row["name"]?></h1>
+				      	
+                              </div>
+                            <?php
+                              }
+                            } else {
+                              echo "0 results";
+                            }
+                            $conn->close();
+                            ?>
