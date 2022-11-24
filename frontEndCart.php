6d66d9058a8b157212746d3e593ec56e476ab385
Andrew Soltis <ams.soltis@ou.edu>
2022-11-24 09:29:06 -0600

    Test

--- frontEndCart.php
+++ frontEndCart.php
@@ -1,91 +1,31 @@
-<?php
+ <?php
                         $servername = "localhost";
                         $username = "asoltiso_project";
                         $password = "Project1243";
                         $dbname = "asoltiso_project";   
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+$iid = $_GET['id'];
+//echo $iid;
+$sql = "Select * From product Where productID =".$iid;
+//echo $sql;
+    $result = $conn->query($sql);
 
-    $product_id = (int)$_GET['product_id'];
-    $quantity = (int)$_GET['quantity'];
-    // Prepare the SQL statement, we basically are checking if the product exists in our databaser
-    $sql = "Select * From product Where productID =".$product_id;
-    $stmt = $conn->query($sql);
-    // Fetch the product from the database and return the result as an Array
-    $product =  $conn->query($sql);
-    // Check if the product exists (array is not empty)
-    if ($product && $quantity > 0) {
-        // Product exists in database, now we can create/update the session variable for the cart
-        if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
-            if (array_key_exists($product_id, $_SESSION['cart'])) {
-                // Product exists in cart so just update the quanity
-                $_SESSION['cart'][$product_id] += $quantity;
-            } else {
-                // Product is not in cart so add it
-                $_SESSION['cart'][$product_id] = $quantity;
-            }
-        } else {
-            // There are no products in cart, this will add the first product to cart
-            $_SESSION['cart'] = array($product_id => $quantity);
-        }
-    }
-    // Prevent form resubmission...
-    //header('location: index.php?page=cart');
-    //exit;
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
 ?>
+                <h1><?=$row["name"]?></h1>
 
-<div class="cart content-wrapper">
-    <h1>Shopping Cart</h1>
-    <form action="index.php?page=cart" method="post">
-        <table>
-            <thead>
-                <tr>
-                    <td colspan="2">Product</td>
-                    <td>Price</td>
-                    <td>Quantity</td>
-                    <td>Total</td>
-                </tr>
-            </thead>
-            <tbody>
-                <?php if (empty($products)): ?>
-                <tr>
-                    <td colspan="5" style="text-align:center;">You have no products added in your Shopping Cart</td>
-                </tr>
-                <?php else: ?>
-                <?php foreach ($products as $product): ?>
-                <tr>
-                    <td class="img">
-                        <a href="index.php?page=product&id=<?=$product['id']?>">
-                            <img src="imgs/<?=$product['img']?>" width="50" height="50" alt="<?=$product['name']?>">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="index.php?page=product&id=<?=$product['id']?>"><?=$product['name']?></a>
-                        <br>
-                        <a href="index.php?page=cart&remove=<?=$product['id']?>" class="remove">Remove</a>
-                    </td>
-                    <td class="price">&dollar;<?=$product['price']?></td>
-                    <td class="quantity">
-                        <input type="number" name="quantity-<?=$product['id']?>" value="<?=$products_in_cart[$product['id']]?>" min="1" max="<?=$product['quantity']?>" placeholder="Quantity" required>
-                    </td>
-                    <td class="price">&dollar;<?=$product['price'] * $products_in_cart[$product['id']]?></td>
-                </tr>
-                <?php endforeach; ?>
-                <?php endif; ?>
-            </tbody>
-        </table>
-        <div class="subtotal">
-            <span class="text">Subtotal</span>
-            <span class="price">&dollar;<?=$subtotal?></span>
-        </div>
-        <div class="buttons">
-            <input type="submit" value="Update" name="update">
-            <input type="submit" value="Place Order" name="placeorder">
-        </div>
-    </form>
-</div>
+ <?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
