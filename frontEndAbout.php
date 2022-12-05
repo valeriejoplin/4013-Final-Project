9eb5f78af19c70baeb8dba3ee7d40e17d6b5abde
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-12-05 14:22:24 -0600

    Create frontEndAbout.php

---
+++ frontEndAbout.php
@@ -0,0 +1,12 @@
+<h1 style=text-align:center;> About Us </h1>
+<table>
+  <tr>
+    <td><img src="https://www.number13.de/content/images/2019/04/Factory-Town.jpg" width="300"></td>
+    <td><img src="https://www.number13.de/content/images/2019/03/League-of-Legends.jpg" width="300"></td>
+  </tr>
+  
+  <tr>
+    <td><img src="https://www.number13.de/content/images/2019/07/Shakes-and-Fidget.jpg" width="300" ></td>
+    <td><img src="https://www.number13.de/content/images/2019/07/Eco.jpg" width="300"></td>
+  </tr>
+</table>
