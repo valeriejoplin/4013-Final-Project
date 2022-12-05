ddaacfc4c28f29f560fe68fcd290e95a822db69c
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-12-05 14:58:13 -0600

    .

--- addproduct.php
+++ addproduct.php
@@ -22,6 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   }
 }
+?>
 
 
 
@@ -51,4 +52,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
-  ?>
+  
