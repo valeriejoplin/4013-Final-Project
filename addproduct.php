0de6765d7bcf8814fa1b069919a3ab32a3a5f75d
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-12-05 15:02:03 -0600

    pop up to work

--- addproduct.php
+++ addproduct.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
-<div class="modal fade" id="addInstructor" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addInstructorLabel" aria-hidden="true">
+<div class="modal fade" id="addproduct" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addInstructorLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
