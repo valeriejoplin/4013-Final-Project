6171a546c790354659e4ae114484b3e516ddb6d0
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-12-07 13:31:57 -0600

    Update frontEndContact.php

--- frontEndContact.php
+++ frontEndContact.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
+    <title>Front End Home</title>
+
+</head>
  <?php require_once("frontEndHeader.php"); ?>
              <form action="https://formsubmit.co/3cbd6087d29d14ef54538c6817c28eba" method="POST">
 
