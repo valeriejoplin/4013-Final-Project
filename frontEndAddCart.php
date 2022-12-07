4dd995647eb0feac532b9c56246cc1a860854c9d
PC <PC@ANDREW-DESKTOP>
2022-12-06 20:55:52 -0600

    test

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -1,11 +1,5 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
-    <title>Cart</title>
-</head>
+
 <?php // PHP part
     session_set_cookie_params(0);
     session_start();          // Start the session
@@ -18,6 +12,13 @@
     array_push($_SESSION['cart'],$product, $quantity);   
     //print_r($_SESSION['cart']);
 ?>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
+    <title>Cart</title>
+</head>
  <body>
      <div class="container">
         <?php require_once("frontEndHeader.php"); ?>
