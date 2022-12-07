e0249dbcf24631557fa3b8bd2b83b80ebd2d7e3f
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-12-06 19:21:47 -0600

    trying edit feature

--- addproduct.php
+++ addproduct.php
@@ -6,8 +6,11 @@
         Add New
       </button>
 
+<?php
+$sql = "SELECT * from Product";
+$result = $conn->query($sql);
 
-
+?>
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -18,6 +21,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmtAdd->bind_param("sissi", $_POST['iprodname'], $_POST['iprice'], $_POST['ishortdesc'], $_POST['ilongdesc'],$_POST['iqty']);
         $stmtAdd->execute();   
       echo '<div class="alert alert-success" role="alert">New Item added.</div>';
+      break;
+
+
+      case 'Edit':
+      $sqlEdit = "update Product set football_name=?, city_ID=? where city_ID=?";
+      $stmtEdit = $conn->prepare($sqlEdit);
+      $stmtEdit->bind_param("ssi", $_POST['editteamname'], $_POST['editcityID'], $_POST['iid']);
+      $stmtEdit->execute();
+      echo '<div class="alert alert-success" role="alert">Team edited.</div>';
       break;
 
   }
@@ -59,3 +71,62 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
   
+<?php
+
+
+
+
+    if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+          
+          <tr>
+            <td><?=$row["productID"]?></td>
+            <td><?=$row["name"]?></td>
+            
+            <td>
+              <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editfootball<?=$row["city_ID"]?>" style = "background-color:white;">
+                Edit
+              </button>
+              <div class="modal fade" id="editfootball<?=$row["city_ID"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editfootball<?=$row["city_ID"]?>Label" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="editfootball<?=$row["city_ID"]?>Label">Edit team</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form method="post" action="">
+                        <div class="mb-3">
+                          <label for="editfootball<?=$row["city_ID"]?>Name" class="form-label">Enter CityID</label>
+                             <input type="text" class="form-control" id="editfootball<?=$row["city_ID"]?>Name" aria-describedby="editfootball<?=$row["city_ID"]?>Help" name="editcityID" value="<?=$row['city_ID']?>">
+                             <label for="editfootball<?=$row["city_ID"]?>Name" class="form-label">Enter Team Name</label>
+                             <input type="text" class="form-control" id="editfootball<?=$row["city_ID"]?>Name" aria-describedby="editfootball<?=$row["city_ID"]?>Help" name="editteamname" value="<?=$row['football_name']?>">
+                             <div id="editfootball<?=$row["city_ID"]?>Help" class="form-text">Enter the Teams CityID and Name.</div>
+                            </div>
+                        <input type="hidden" name="iid" value="<?=$row['city_ID']?>">
+                        <input type="hidden" name="saveType" value="Edit">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <form method="post" action="">
+                <input type="hidden" name="iid" value="<?=$row["city_ID"]?>" />
+                <input type="hidden" name="saveType" value="Delete">
+                <button type="submit" class="btn" onclick="return confirm('Are you sure?')"style = "background-color:white;"> Delete </button>
+              </form>
+            </td>
+          </tr>
+          
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
