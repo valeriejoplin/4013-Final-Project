9fbb1f09e0504b94933dbd85016aa1b52816d1af
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-12-07 12:03:50 -0600

    Update frontendfooter.php

--- frontendfooter.php
+++ frontendfooter.php
@@ -1,26 +1,17 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-<style>
-.icons{
-            width:100px;
-            height:100px;
-            }
-.footericon{
-            width:100px;
-            height:100px;
-            }
-            </style>                 
+             
 <footer>
-            <div class="icons">
+            <div class="icons" style="width:100px;height:100px;">
                 <img class="footericon" src="assets/send-mail.svg" />
                 <img class="footericon" src="assets/phone.svg" />
                 <img class="footericon" src="assets/message-text.svg" />
                 <a class="footericon" href="https://www.facebook.com/" target="_blank">
                     <img src="assets/facebook.svg" />
                 </a>
-                <a class="footericon" href="https://www.twitter.com/" target="_blank">
+                <a class="footericon style="width:100px;height:100px;" href="https://www.twitter.com/" target="_blank">
                     <img src="assets/twitter.svg" />
                 </a>
-                <a class="footericon" href="https://www.instagram.com/" target="_blank">
+                <a class="footericon style="width:100px;height:100px;" href="https://www.instagram.com/" target="_blank">
                     <img src="assets/instagram.svg" />
                 </a>
             </div>
