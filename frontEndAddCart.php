12aec5dc2a7d9bd4df2b8af21204fccd3a04ff83
PC <PC@ANDREW-DESKTOP>
2022-12-06 21:22:25 -0600

    test

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -18,9 +18,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="auto-complete.js"></script>
     <title>Cart</title>
 </head>
- <body>
+<body>
      <div class="container">
         <?php require_once("frontEndHeader.php"); ?>
 
@@ -35,4 +37,49 @@
              }  ?>
             </tr>
         </table>
-</div>
+    </div>
+        <div class="container">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h2 class="panel-title">Add your Address</h2>
+            </div>
+            <div class="panel-body">
+                <input id="autocomplete" placeholder="Enter your address" onFocus="geolocate()" type="text" class="form-control">
+                <div id="address">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label class="control-label">Address</label>
+                            <input class="form-control" id="street_number" disabled="true">
+                        </div>
+                        <div class="col-md-6">
+                            <label class="control-label">Street</label>
+                            <input class="form-control" id="route" disabled="true">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label class="control-label">City</label>
+                            <input class="form-control field" id="locality" disabled="true">
+                        </div>
+                        <div class="col-md-6">
+                            <label class="control-label">State</label>
+                            <input class="form-control" id="administrative_area_level_1" disabled="true">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label class="control-label">Zip code</label>
+                            <input class="form-control" id="postal_code" disabled="true">
+                        </div>
+                        <div class="col-md-6">
+                            <label class="control-label">Country</label>
+                            <input class="form-control" id="country" disabled="true">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCMCbt2oQ2t25_-x-Tbk7Ny6OOtzvuW9rY&libraries=places&callback=initAutocomplete" async defer></script>
+<body>
