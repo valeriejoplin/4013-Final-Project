46a54242e913805e189b375d8a44228d754ccce9
PC <PC@ANDREW-DESKTOP>
2022-12-06 20:47:57 -0600

    Andrew Changes Front End Added Php header file

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
+    <title>Cart</title>
+</head>
+<style>
+
 <?php // PHP part
     session_set_cookie_params(0);
     session_start();          // Start the session
@@ -10,17 +22,19 @@
     array_push($_SESSION['cart'],$product, $quantity);   
     //print_r($_SESSION['cart']);
 ?>
-                                    
-<?php require_once("frontEndHeader.php"); ?>
+ <body>
+     <div class="container">
+        <?php require_once("frontEndHeader.php"); ?>
 
-<table class="table">     <!-- HTML Part (optional) -->
-    <tr>
-      <th>Product</th>
-      <th>Quantity</th>
-    </tr>                                      
-    <tr>
-     <?php for($i = 0 ; $i < count($_SESSION['cart']) ; $i++) {
-     echo '<td>'.$_SESSION['cart'][$i].'</td>';
-     }  ?>
-    </tr>
-</table>
+        <table class="table">     <!-- HTML Part (optional) -->
+            <tr>
+              <th>Product</th>
+              <th>Quantity</th>
+            </tr>                                      
+            <tr>
+             <?php for($i = 0 ; $i < count($_SESSION['cart']) ; $i++) {
+             echo '<td>'.$_SESSION['cart'][$i].'</td>';
+             }  ?>
+            </tr>
+        </table>
+</div>
