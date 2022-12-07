bc267c4fa06d7b9654e7bb29e2d4cdeb172c41a6
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-12-06 21:49:44 -0600

    again for main

--- frontEndMain.php
+++ frontEndMain.php
@@ -287,9 +287,21 @@ document.querySelector("#close").addEventListener("click", function(){
     </div>
 
 <footer>
-
-<?php require_once("frontendfooter.php"); ?>
-
-</footer>
+            <div class="icons">
+                <img class="footericon" src="assets/send-mail.svg" />
+                <img class="footericon" src="assets/phone.svg" />
+                <img class="footericon" src="assets/message-text.svg" />
+                <a class="footericon" href="https://www.facebook.com/" target="_blank">
+                    <img src="assets/facebook.svg" />
+                </a>
+                <a class="footericon" href="https://www.twitter.com/" target="_blank">
+                    <img src="assets/twitter.svg" />
+                </a>
+                <a class="footericon" href="https://www.instagram.com/" target="_blank">
+                    <img src="assets/instagram.svg" />
+                </a>
+            </div>
+            <p>VAST Fashion<p>
+        </footer>
 </body>
 </html>
