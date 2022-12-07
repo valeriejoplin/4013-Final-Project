f2c7cf5763d48605b0a5d4be7c3ab29a52f577ff
PC <PC@ANDREW-DESKTOP>
2022-12-07 00:26:29 -0600

    g

--- frontEndHeader.php
+++ frontEndHeader.php
@@ -19,6 +19,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="frontEndAddCart.php">Cart</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="frontEndContact.php">Contact</a>
+                    </li>
                 </ul>
             </div>
         </nav>
