e34ff7cff9163b3320dcdead08c3864507ac07f4
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-12-07 12:30:25 -0600

    Update frontEndContact.php

---
+++ frontEndContact.php
@@ -0,0 +1,11 @@
+ <?php require_once("frontEndHeader.php"); ?>
+             <form action="https://formsubmit.co/3cbd6087d29d14ef54538c6817c28eba" method="POST">
+
+                <input type="hidden" name="_autoresponse" value="Thank you for your inquirie to VAST someone will never get back to you!">
+                <input type="hidden" name="_next" value="https://project.asoltis.oucreate.com/frontEndMain.php">
+                <p>Name:</p>
+                <input type="text" name="Feedback" required>
+                <p>Email</p>
+                <input type="email" name="email" placeholder="Email Address">
+                <button  style="width:80%"type="submit">Send</button>
+            </form>
