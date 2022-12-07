b5429b09e9f7536a2c7bb5ac3c7426de0dec16de
PC <PC@ANDREW-DESKTOP>
2022-12-06 21:59:32 -0600

    test

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -38,4 +38,25 @@
             </tr>
         </table>
 </div>
+<button id="openFormButton">Check Out</button>
+  <form id="addressForm" style="display:none;">
+    <label for="name">Name:</label><br>
+    <input type="text" id="name" name="name"><br>
+    <label for="address">Address:</label><br>
+    <input type="text" id="address" name="address"><br>
+    <label for="city">City:</label><br>
+    <input type="text" id="city" name="city"><br>
+    <label for="state">State:</label><br>
+    <input type="text" id="state" name="state"><br>
+    <label for="zip">Zip Code:</label><br>
+    <input type="text" id="zip" name="zip"><br><br>
+    <input type="submit" value="Submit">
+  </form>
+  <script>
+    var openFormButton = document.getElementById('openFormButton');
+    var addressForm = document.getElementById('addressForm');
+    openFormButton.addEventListener('click', function() {
+      addressForm.style.display = 'block';
+    });
+  </script>
 
