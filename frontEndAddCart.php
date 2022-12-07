3eddb3735c20c8b16b9f3e19b3f5d72b146ffb81
PC <PC@ANDREW-DESKTOP>
2022-12-06 21:39:06 -0600

    cart fix

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -25,48 +25,5 @@
              }  ?>
             </tr>
         </table>
-    </div>
+</div>
 
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h2 class="panel-title">Add your Address</h2>
-            </div>
-            <div class="panel-body">
-                <input id="autocomplete" placeholder="Enter your address" onFocus="geolocate()" type="text" class="form-control">
-                <div id="address">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label class="control-label">Address</label>
-                            <input class="form-control" id="street_number" disabled="true">
-                        </div>
-                        <div class="col-md-6">
-                            <label class="control-label">Street</label>
-                            <input class="form-control" id="route" disabled="true">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label class="control-label">City</label>
-                            <input class="form-control field" id="locality" disabled="true">
-                        </div>
-                        <div class="col-md-6">
-                            <label class="control-label">State</label>
-                            <input class="form-control" id="administrative_area_level_1" disabled="true">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label class="control-label">Zip code</label>
-                            <input class="form-control" id="postal_code" disabled="true">
-                        </div>
-                        <div class="col-md-6">
-                            <label class="control-label">Country</label>
-                            <input class="form-control" id="country" disabled="true">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCMCbt2oQ2t25_-x-Tbk7Ny6OOtzvuW9rY&libraries=places&callback=initAutocomplete" async defer></script>
