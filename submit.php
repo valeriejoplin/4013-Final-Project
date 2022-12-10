5aeaa0e78e1e29b1bad9dfe2128aa455e9417c84
PC <PC@ANDREW-DESKTOP>
2022-12-10 15:51:29 -0600

    test

--- submit.php
+++ submit.php
@@ -22,7 +22,7 @@ if ($conn->connect_error) {
 }
 
 // Create an SQL insert query
-        $sql = "INSERT INTO order (Name, Address, City, State, Zip) values ($name, $address, $city, $state, $zip)";
+        $sql = "INSERT INTO order (orderID, Name, Address, City, State, Zip) values (NULL, $name, $address, $city, $state, $zip)";
 ?>
 <?php require_once("frontEndHeader.php"); ?>
 <H1> Order Placed Successfully <h1>
