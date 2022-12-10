ea5db90ff6410531d7fa57b36b4f6ce450f2c615
PC <PC@ANDREW-DESKTOP>
2022-12-10 15:48:08 -0600

    t

--- submit.php
+++ submit.php
@@ -3,11 +3,11 @@
     // submit.php
 
 // Get values from the form
-//$name = $_POST['name'];
-//$address = $_POST['address'];
-//$city = $_POST['city'];
-//$state = $_POST['state'];
-//$zip = $_POST['zip'];
+$name = $_POST['name'];
+$address = $_POST['address'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zip = $_POST['zip'];
 
 // Connect to the database
                         $servername = "165.227.18.177";
@@ -22,9 +22,9 @@ if ($conn->connect_error) {
 }
 
 // Create an SQL insert query
-        $sqlAdd = "INSERT INTO order (Name, Address, City, State, Zip) values (?, ?, ?, ?, ?)";
+        $sqlAdd = "INSERT INTO order (orderID, Name, Address, City, State, Zip) values (?, ?, ?, ?, ?, ?)";
         $stmtAdd = $conn->prepare($sqlAdd);
-        $stmtAdd->bind_param("sssss", $_POST['name'], $_POST['address'], $_POST['city'], $_POST['state'], $_POST['zip']);
+        $stmtAdd->bind_param("isssss", 1, name, address, city, state, zip);
         $stmtAdd->execute(); 
 ?>
 <?php require_once("frontEndHeader.php"); ?>
