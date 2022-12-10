ff6c41b138ad792162107aebe832fb35b5b93bb5
PC <PC@ANDREW-DESKTOP>
2022-12-10 15:08:24 -0600

    test

--- submit.php
+++ submit.php
@@ -1,11 +1,11 @@
 <?php
-
+  header('Location: frontEndMain.php');
+  exit;
     echo "hello".$_POST['name'];
     // Clear the session data
     //$_SESSION = array();
     //session_destroy();
 
   //Redirect to the home page
-  header('Location: frontEndMain.php');
-  exit;
+
 ?>
