f08a29747cac7a28a54aadda49c2fd5ce8bde695
PC <PC@ANDREW-DESKTOP>
2022-12-10 15:06:11 -0600

    test

---
+++ submit.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo "hello".$_POST['name'];
+    // Clear the session data
+    //$_SESSION = array();
+    //session_destroy();
+
+  // Redirect to the home page
+  //header('Location: frontEndMain.php');
+  //exit;
+}
+?>
