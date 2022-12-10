0184aee420aebbe4e5ed7a123bde3fbe40bac53e
PC <PC@ANDREW-DESKTOP>
2022-12-10 15:24:12 -0600

    test

--- submit.php
+++ submit.php
@@ -22,7 +22,7 @@ if ($conn->connect_error) {
 }
 
 // Create an SQL insert query
-$sql = "INSERT INTO order (Name, Address, City, State, Zip)
+$sql = "INSERT INTO order ('Name', 'Address', 'City', 'State', 'Zip')
         VALUES ('$name', '$address', '$city', '$state', '$zip')";
 
 mysqli_close($conn);
