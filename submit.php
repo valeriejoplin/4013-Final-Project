0b5ff7663d32edbcbbfac5cf537098a5c0e3a9e4
PC <PC@ANDREW-DESKTOP>
2022-12-10 15:10:33 -0600

    test

--- submit.php
+++ submit.php
@@ -1,11 +1,43 @@
 <?php
-  header('Location: frontEndMain.php');
-  exit;
-    echo "hello".$_POST['name'];
+
+    // submit.php
+
+// Get values from the form
+$name = $_POST['name'];
+$address = $_POST['address'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zip = $_POST['zip'];
+
+// Connect to the database
+                        $servername = "165.227.18.177";
+                        $username = "asoltiso_project";
+                        $password = "Project1243";
+                        $dbname = "asoltiso_project";   
+
+$conn = mysqli_connect($host, $user, $password, $dbname);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Create an SQL insert query
+$sql = "INSERT INTO table_name (Name, Address, City, State, Zip)
+        VALUES ('$name', '$address', '$city', '$state', '$zip')";
+
+// Execute the query
+if (mysqli_query($conn, $sql)) {
+  echo "Record added successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
     // Clear the session data
-    //$_SESSION = array();
-    //session_destroy();
+    $_SESSION = array();
+    session_destroy();
 
   //Redirect to the home page
-
+  header('Location: frontEndMain.php');
+  exit;
 ?>
