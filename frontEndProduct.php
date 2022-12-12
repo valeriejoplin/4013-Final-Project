70f3f872750a76ff853864caa883458c0fc82ef3
amsso <amsso@ANDREW-DESKTOP>
2022-12-12 01:09:35 -0600

    post

--- frontEndProduct.php
+++ frontEndProduct.php
@@ -116,7 +116,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$iid = $_GET['id'];
+$iid = $_POST['id'];
 //echo $iid;
 $sql = "Select * From product Where productID =".$iid;
 //echo $sql;
