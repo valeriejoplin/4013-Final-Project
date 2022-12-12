aede9237d3a74ea0d4e1f5139729daaeb7b79a78
amsso <amsso@ANDREW-DESKTOP>
2022-12-11 19:56:01 -0600

    tesr

--- submit.php
+++ submit.php
@@ -16,3 +16,26 @@ $zip = $_POST['zip'];
 <?php echo "<H2>$name<H2>" ?>
 <?php echo "$address"." "."$city".", "."$state"." "."$zip"?>
 
+<?php 
+    $servername = "165.227.18.177";
+    $username = "asoltiso_project";
+    $password = "Project1243";
+    $dbname = "asoltiso_project";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+    $sql = "INSERT INTO orders (Name, Address, City, State, Zip)
+            VALUES ('$name', '$address', '$city', '$state', '$zip')";
+    if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+    } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+?>
