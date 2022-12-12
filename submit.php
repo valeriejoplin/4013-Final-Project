56fcfbff4678cae26e8b19e6660acd165020211d
amsso <amsso@ANDREW-DESKTOP>
2022-12-11 19:59:58 -0600

    test

--- submit.php
+++ submit.php
@@ -27,6 +27,17 @@ $servername = "165.227.18.177";
     $conn->close();
 ?>
 
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="./assets/catalogStyle.css" rel="stylesheet"/>
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
+    <script src="assets/javascript/w3.js"></script>
+    <title>Catalog</title>
+<head>
+
 <?php require_once("frontEndHeader.php"); ?>
 
 <H1> Order Placed Successfully <h1>
