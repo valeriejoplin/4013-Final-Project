9e60712582dd3e7368e60271ffc9ef91545ad540
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-12-12 08:37:17 -0600

    test

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -71,12 +71,14 @@ if (isset($_SESSION['cart'])) {
                               // output data of each row
                               while($row = $result->fetch_assoc()) {
                             ?>
-                                <img src="assets/<?=$row["img"]?>.png" </img>
-                               <h5 class="card-title"><?=$row["name"]?></h5>
-                                <p class="card-text">
-                                Quantity: <?php echo $quantity; ?><br>
-                                Price: $<?=$row["price"]?><br>
-                                </p>
+                                <img style="width:100px; float:left;"src="assets/<?=$row["img"]?>.png" </img>
+                                <div class="details">
+                                   <h5 class="card-title"><?=$row["name"]?></h5>
+                                    <p class="card-text">
+                                    Quantity: <?php echo $quantity; ?><br>
+                                    Price: $<?=$row["price"]?><br>
+                                    </p>
+                                </div>
                             <?php
                               }
                             } else {
