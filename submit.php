0d6c18f745ddf47f5801014d214d64ed14109d65
amsso <amsso@ANDREW-DESKTOP>
2022-12-11 20:10:17 -0600

    test

--- submit.php
+++ submit.php
@@ -48,6 +48,36 @@ $zip = $_POST['zip'];
 
             $conn->close();
         ?>
+         <?php
+                        $servername = "165.227.18.177";
+                        $username = "asoltiso_project";
+                        $password = "Project1243";
+                        $dbname = "asoltiso_project";   
+						
+
+                            // Create connection
+                            $conn = new mysqli($servername, $username, $password, $dbname);
+                            // Check connection
+                            if ($conn->connect_error) {
+                              die("Connection failed: " . $conn->connect_error);
+                            }
+
+                            $sql = "SELECT * FROM orders where Name="$name" Order by orderID desc Limit 1";
+                            //echo $sql;
+                            $result = $conn->query($sql);
+
+                            if ($result->num_rows > 0) {
+                              // output data of each row
+                              while($row = $result->fetch_assoc()) {
+                            ?>
+                              <h1><?=$row["orderID"]?><h1>
+                            <?php
+                              }
+                            } else {
+                              echo "0 results";
+                            }
+                            $conn->close();
+                            ?>
         <?php echo "<H2>$name<H2>" ?>
         <?php echo "$address"." "."$city".", "."$state"." "."$zip"?>
     </div>
