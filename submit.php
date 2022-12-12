226767bcf5097733b5c1dfa75935a90a61f6bf94
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-12-11 19:50:15 -0600

    Update submit.php

--- submit.php
+++ submit.php
@@ -8,13 +8,11 @@ $address = $_POST['address'];
 $city = $_POST['city'];
 $state = $_POST['state'];
 $zip = $_POST['zip'];
-
 ?>
 
-echo
 <?php require_once("frontEndHeader.php"); ?>
 
 <H1> Order Placed Successfully <h1>
 <?php echo "<H2>$name<H2>" ?>
-<?php echo "$address"." "."$city".","."$state"." "."$zip"?>
+<?php echo "$address"." "."$city".", "."$state"." "."$zip"?>
 
