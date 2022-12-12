44b622af9e88715f9e571465f26f223eb0f81909
amsso <amsso@ANDREW-DESKTOP>
2022-12-11 21:06:50 -0600

    f

--- frontEndProduct.php
+++ frontEndProduct.php
@@ -7,7 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
     <script src="assets/javascript/w3.js"></script>
-    <title></title>
     <title>Details</title>
 
     <style>
