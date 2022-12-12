b82ccf4fd67317a6cbad4d72ea17fc9b79be81c6
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-12-12 03:34:27 -0600

    Create login.php

---
+++ login.php
@@ -0,0 +1,7 @@
+<form method="POST" action="login.php">
+    <label for="username">Username:</label><br>
+    <input type="text" id="username" name="username"><br>
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password"><br><br>
+    <input type="submit" value="Submit">
+</form>
