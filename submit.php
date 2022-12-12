7ace3f1a79f2d2358f3146ece538b545403feb74
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-12-11 19:46:07 -0600

    Update submit.php

--- submit.php
+++ submit.php
@@ -9,30 +9,11 @@ $city = $_POST['city'];
 $state = $_POST['state'];
 $zip = $_POST['zip'];
 
-// Connect to the database
-                        $servername = "165.227.18.177";
-                        $username = "asoltiso_project";
-                        $password = "Project1243";
-                        $dbname = "asoltiso_project";   
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-// Create an SQL insert query
-$sqlAdd = "INSERT INTO order (Name, Address, City, State, Zip) values (?, ?, ?, ?, ?)";
-
-// Prepare the SQL statement for execution
-$stmtAdd = $conn->prepare($sqlAdd);
-
-// Bind the values from the form to the query parameters
-$stmtAdd->bind_param("sssss", $_POST['name'], $_POST['address'], $_POST['city'], $_POST['state'], $_POST['zip']);
-
-// Execute the SQL statement
-$stmtAdd->execute();
-
 ?>
+
+echo
 <?php require_once("frontEndHeader.php"); ?>
+
 <H1> Order Placed Successfully <h1>
+<?php echo "$name" ?>
+
