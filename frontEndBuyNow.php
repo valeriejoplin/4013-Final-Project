53cfffe0a85e39974e0bffa39229c22cf5a89aa7
amsso <amsso@ANDREW-DESKTOP>
2022-12-12 16:34:54 -0600

    t

--- frontEndBuyNow.php
+++ frontEndBuyNow.php
@@ -42,14 +42,12 @@ $quantity = $_POST['quantity'];
             if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
             }
-            if (!empty($_SESSION['cart'])) {
             $sql = "INSERT INTO orders (Name, Address, City, State, Zip)
                     VALUES ('$name', '$address', '$city', '$state', '$zip')";
             if ($conn->query($sql) === TRUE) {
             echo "<h1>Successfully Submitted Order<h1>";
             } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
-            }
             }
             $conn->close();
         ?>
