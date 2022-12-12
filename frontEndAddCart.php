5b44e06c8d7a22ac4babf43b87cfc27c8ff47478
amsso <amsso@ANDREW-DESKTOP>
2022-12-11 20:24:15 -0600

    test

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -50,26 +50,31 @@ border: 1px solid black;
         </table>
 
 <button id="openFormButton">Check Out</button>
-<form id="addressForm" action="submit.php" method="POST" style="display:none;">
-      <label for="name">Name:</label><br>
-      <input type="text" id="name" name="name"><br>
-      <label for="address">Address:</label><br>
-      <input type="text" id="address" name="address"><br>
-      <label for="city">City:</label><br>
-      <input type="text" id="city" name="city"><br>
-      <label for="state">State:</label><br>
-      <input type="text" id="state" name="state"><br>
-      <label for="zip">Zip Code:</label><br>
-      <input type="text" id="zip" name="zip"><br><br>
-      <input type="submit" value="Submit">
-    </form>
-  <script>
-    var openFormButton = document.getElementById('openFormButton');
-    var addressForm = document.getElementById('addressForm');
-    openFormButton.addEventListener('click', function() {
-      addressForm.style.display = 'block';
-    });
-  </script>
+<form id="addressForm" action="frontEndOrderPlaced.php" method="POST" class="hidden">
+  <label for="name">Name:</label><br>
+  <input type="text" id="name" name="name"><br>
+  <label for="address">Address:</label><br>
+  <input type="text" id="address" name="address"><br>
+  <label for="city">City:</label><br>
+  <input type="text" id="city" name="city"><br>
+  <label for="state">State:</label><br>
+  <input type="text" id="state" name="state"><br>
+  <label for="zip">Zip Code:</label><br>
+  <input type="text" id="zip" name="zip"><br><br>
+  <input type="submit" value="Submit">
+</form>
+<style>
+  .hidden {
+    display: none;
+  }
+</style>
+<script>
+  var openFormButton = document.getElementById('openFormButton');
+  var addressForm = document.getElementById('addressForm');
+  openFormButton.addEventListener('click', function() {
+    addressForm.classList.toggle('hidden');
+  });
+</script>
 
   <a class="keepShopping" id="keepShopping" href="/frontEndCatalog.php">Keep Shopping</a>
   
