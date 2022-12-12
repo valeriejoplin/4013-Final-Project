c7b97dffc6385a8f2a7e106cb4dc330650c5f1b6
amsso <amsso@ANDREW-DESKTOP>
2022-12-11 21:36:49 -0600

    T

--- filtered.php
+++ filtered.php
@@ -1,1 +1,11 @@
-<?php echo$_Post['brands'] ?>
+<?php   
+  $selectedBrand = $_POST["brands"];
+  $selectedCategory = $_POST["categories"];
+  $selectedItem = $_POST["items"];
+
+  // Output the selected values
+  echo "You selected the following options:<br>";
+  echo "Brand: " . $selectedBrand . "<br>";
+  echo "Category: " . $selectedCategory . "<br>";
+  echo "Item: " . $selectedItem;
+  ?>
