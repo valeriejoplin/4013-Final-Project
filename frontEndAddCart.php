0e84b58eea0114b8c11db1b3fdd6999e5aee3836
amsso <amsso@ANDREW-DESKTOP>
2022-12-11 22:49:09 -0600

    test

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -1,18 +1,16 @@
-<?php
-  // PHP part
-  session_set_cookie_params(0);
-  session_start(); // Start the session
+<?php // PHP part
+    session_set_cookie_params(0);
+    session_start();          // Start the session
+    
+    if(empty($_SESSION['cart']))
+	{
+		$_SESSION['cart'] = array();
+    }
 
-  if (!isset($_SESSION['cart'])) {
-    // Set the 'cart' key to an empty array if it doesn't exist
-    $_SESSION['cart'] = array();
-  }
-
-  $product = $_POST['product_id'];
-  $quantity = $_POST['quantity'];
-  array_push($_SESSION['cart'], $product, $quantity);
-
-  //print_r($_SESSION['cart']);
+    $product=$_POST['product_id'];
+	$quantity=$_POST['quantity'];
+    array_push($_SESSION['cart'],$product, $quantity);   
+    //print_r($_SESSION['cart']);
 ?>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
@@ -40,6 +38,12 @@ border: 1px solid black;
     <?php require_once("frontEndHeader.php"); ?>
 
     <div class="card-deck">
+    <?php
+        if(empty($_SESSION['cart']))
+	{
+		break;
+    }
+    ?>
         <?php for($i = 0 ; $i < count($_SESSION['cart']) ; $i=$i+2) { ?>
             <div class="card">
                 <div class="card-body">
@@ -52,6 +56,7 @@ border: 1px solid black;
                 </div>
             </div>
         <?php } ?>
+
     </div>
 
 <button id="openFormButton">Check Out</button>
