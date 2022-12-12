68d1d36ad8051eec2cfdda9456286f41e16f8838
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-12-12 03:34:02 -0600

    Update backEndPassword.php
    B

--- backEndPassword.php
+++ backEndPassword.php
@@ -1,28 +1,7 @@
-<!DOCTYPE html>
-<html>
-<body>
-<?php
-// Start a session
-session_start();
-
-// Prompt the user for a username and password
-echo "Please enter your username and password:";
-$username = readline("Username: ");
-$password = readline("Password: ");
-
-// Check if the entered username and password are correct
-if ($username == "admin" && $password == "admin") {
-    // Set the login status to true
-    $_SESSION["loggedIn"] = true;
-
-    // Redirect the user to the backEndMain.php file
-    header("Location: backEndMain.php");
-    exit;
-} else {
-    // Display an error message if the username or password is incorrect
-    echo "Invalid username or password. The correct username and password is admin and admin";
-}
-
-?>
-</body>
-</html>
+<form method="POST" action="login.php">
+    <label for="username">Username:</label><br>
+    <input type="text" id="username" name="username"><br>
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password"><br><br>
+    <input type="submit" value="Submit">
+</form>
