feafec81316ce8bcf5392eec922d9f5e417b8fa8
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-12-12 03:36:47 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,26 @@
-<form method="POST" action="login.php">
-    <label for="username">Username:</label><br>
-    <input type="text" id="username" name="username"><br>
-    <label for="password">Password:</label><br>
-    <input type="password" id="password" name="password"><br><br>
-    <input type="submit" value="Submit">
-</form>
+<?php
+
+// Start a session
+session_start();
+
+// Check if the form has been submitted
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    // Get the username and password from the form
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Check if the entered username and password are correct
+    if ($username == "admin" && $password == "admin") {
+        // Set the login status to true
+        $_SESSION["loggedIn"] = true;
+
+        // Redirect the user to the backEndMain.php file
+        header("Location: backEndMain.php");
+        exit;
+    } else {
+        // Display an error message if the username or password is incorrect
+        echo "Invalid username or password. The correct username and password is admin and admin";
+    }
+}
+
+?>
