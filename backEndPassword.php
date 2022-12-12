1f4d193cd4e95909bfcfa49980c779c5c6561ba3
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-12-12 03:18:38 -0600

    Update backEndPassword.php

--- backEndPassword.php
+++ backEndPassword.php
@@ -1,42 +1,22 @@
 <?php
 
-$username = '';
-$password = '';
-$errorMessage = '';
+// Start a session
+session_start();
 
-// Check if the form has been submitted
-if (isset($_POST['submit'])) {
-  // Retrieve the username and password from the form
-  $username = $_POST['username'];
-  $password = $_POST['password'];
+// Prompt the user for a username and password
+echo "Please enter your username and password:";
+$username = readline("Username: ");
+$password = readline("Password: ");
 
-  // Check if the username and password are correct
-  if ($username === 'admin' && $password === 'admin') {
-    // Redirect to the backend main page
-    header('Location: backEndMain.php');
+// Check if the entered username and password are correct
+if ($username == "admin" && $password == "admin") {
+    // Set the login status to true
+    $_SESSION["loggedIn"] = true;
+
+    // Redirect the user to the backEndMain.php file
+    header("Location: backEndMain.php");
     exit;
-  } else {
-    // Set an error message
-    $errorMessage = 'Incorrect username or password.';
-  }
+} else {
+    // Display an error message if the username or password is incorrect
+    echo "Invalid username or password.";
 }
-
-?>
-
-<h1>username: admin</h1>
-<h1>password: admin</h1>
-<!-- HTML form for the user to enter their username and password -->
-<form method="post" action="">
-  <label for="username">Username:</label>
-  <input type="text" id="username" name="username" value="<?php echo htmlspecialchars($username); ?>">
-  <br>
-  <label for="password">Password:</label>
-  <input type="password" id="password" name="password">
-  <br>
-  <input type="submit" name="submit" value="Login">
-</form>
-
-<!-- Display the error message, if any -->
-<?php if (!empty($errorMessage)) { ?>
-  <p><?php echo $errorMessage; ?></p>
-<?php } ?>
