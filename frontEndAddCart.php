668bc159c58e76dd9031221b03864e7b068634c7
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-12-12 08:41:44 -0600

    tets

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -71,8 +71,8 @@ if (isset($_SESSION['cart'])) {
                               // output data of each row
                               while($row = $result->fetch_assoc()) {
                             ?>
-                                <div class="details" style="height:100px;">
                                 <img style="width:100px; float:left;"src="assets/<?=$row["img"]?>.png" </img>
+                                <div class="details" style="height:100px;">
                                    <h5 class="card-title"><?=$row["name"]?></h5>
                                     <p class="card-text">
                                     Quantity: <?php echo $quantity; ?><br>
