c5172849f73d28fdbe08a6adc10fee66072a8eaa
amsso <amsso@ANDREW-DESKTOP>
2022-12-11 22:47:45 -0600

    test

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -1,15 +1,18 @@
-<?php // PHP part
-    session_set_cookie_params(0);
-    session_start();          // Start the session
-    
-    if(empty($_SESSION['cart']))
-	{
-		$_SESSION['cart'] = array();
-    }
-    $product=$_POST['product_id'];
-	$quantity=$_POST['quantity'];
-    array_push($_SESSION['cart'],$product, $quantity);   
-    //print_r($_SESSION['cart']);
+<?php
+  // PHP part
+  session_set_cookie_params(0);
+  session_start(); // Start the session
+
+  if (!isset($_SESSION['cart'])) {
+    // Set the 'cart' key to an empty array if it doesn't exist
+    $_SESSION['cart'] = array();
+  }
+
+  $product = $_POST['product_id'];
+  $quantity = $_POST['quantity'];
+  array_push($_SESSION['cart'], $product, $quantity);
+
+  //print_r($_SESSION['cart']);
 ?>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
