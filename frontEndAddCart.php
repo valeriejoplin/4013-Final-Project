01327c0140fb2d5d908704f8f63544e7807a0406
amsso <amsso@ANDREW-DESKTOP>
2022-12-12 01:50:20 -0600

    a

--- frontEndAddCart.php
+++ frontEndAddCart.php
@@ -101,7 +101,7 @@ if (isset($_SESSION['cart'])) {
 
     <button class="cartButton" id="keepShopping" onclick="window.location.href='/frontEndCatalog.php'">Keep Shopping</button>
     <form class="cartButton" action="" method="GET">
-        <button  class="cartButton" type="submit" onclick="<?php session_destroy(); ?>">Empty Cart</button>
+        <button type="submit" onclick="<?php session_destroy(); ?>">Empty Cart</button>
     </form>
 </div>
 
