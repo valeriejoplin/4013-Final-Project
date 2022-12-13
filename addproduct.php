d26778f00dde48af47b732736b9707a394097d0e
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-12-13 14:47:09 -0600

    Update addproduct.php

--- addproduct.php
+++ addproduct.php
@@ -22,7 +22,7 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   switch ($_POST['saveType']) {
     case 'Add':
-        $sqlAdd = "insert into product (name,price,shortDesc,longDesc,qtyavalible) value (?, ?,?,?,?)";
+        $sqlAdd = "insert into product (productID, name,price,shortDesc,longDesc,qtyavalible) value (?,?, ?,?,?,?)";
         $stmtAdd = $conn->prepare($sqlAdd);
         $stmtAdd->bind_param("isissi", $_POST['pID'], $_POST['pname'], $_POST['pprice'], $_POST['pshortdesc'], $_POST['plongdesc'],$_POST['pqty']);
         $stmtAdd->execute();   
