730ccedf1519a278d8f0adf10b31d5a776dc25c3
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-12-14 15:40:59 -0600

    github tab for backend

--- backendheader.php
+++ backendheader.php
@@ -41,6 +41,10 @@
               <li class="nav-item active">
                     <a class="nav-link" href="backEndMain.php">Database Diagram</a>
                 </li>
+                </li>
+              <li class="nav-item active">
+                    <a class="nav-link" href="https://github.com/valeriejoplin/4013-Final-Project">GitHub Repo</a>
+                </li>
             </ul>
         </div>
     </nav>
