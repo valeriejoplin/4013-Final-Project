f923e47a300deaf15c0820daa3d912fc19602251
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-12-14 13:34:43 -0600

    Update addproduct.php

--- addproduct.php
+++ addproduct.php
@@ -3,6 +3,11 @@
 <head>
 <?php require_once("backendheader.php"); ?>
         </head>
+   <style>
+   body {
+            background-image: url('https://www.vestian.com/blog/wp-content/uploads/vestian-marathalli-6.jpg');
+        }
+  </style>
  <body>
     <div class="container">
 
