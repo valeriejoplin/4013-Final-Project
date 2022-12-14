e22077cb9738b75fa0c1da3571a8802a7d7801c0
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-12-14 13:41:55 -0600

    Update addproduct.php

--- addproduct.php
+++ addproduct.php
@@ -8,7 +8,7 @@
             background-image: url('https://www.vestian.com/blog/wp-content/uploads/vestian-marathalli-6.jpg');
         }
 .rows{
-        background-color:#ADC8C8;
+        background-color:#AEEAEA;
            }
   </style>
  <body>
