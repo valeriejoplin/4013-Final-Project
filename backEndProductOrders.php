36729bf44b7fe131a5825bb6e6d9b8899227aefa
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-12-14 13:24:51 -0600

    Create backEndProductOrders.php

---
+++ backEndProductOrders.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+<?php require_once("backendheader.php"); ?>
+        </head> 
+<body>
+        <h2>Order Summary </h2>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Product ID</th>
+      <th>Order ID</th>
+      <th>Quantity Purchased</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+ $servername = "165.227.18.177";
+ $username = "asoltiso_project";
+ $password = "Project1243";
+ $dbname = "asoltiso_project"; 
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * from orders ORDER by orderID";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["orderProductID"]?></td>  
+    <td><?=$row["productID"]?></td>  
+    <td><?=$row["orderID"]?></td>
+    <td><?=$row["quantity"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+  <?php require_once("backendfooter.php"); ?>
