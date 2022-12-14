061f0e081833938763bb5ef49d99ba711800a9e5
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-12-14 15:49:33 -0600

    back end header

--- backEndMain.php
+++ backEndMain.php
@@ -2,6 +2,7 @@
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<?php require_once("backendheader.php"); ?>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -9,34 +10,7 @@
     <title>Back End</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="index.php">Project</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="backEndMain.php">Back End Home</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="addproduct.php">Product Inventory</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="backEndOrders.php">Customer Information</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="backEndProductOrders.php">Order History</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="backEndMain.php">Database Diagram</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="https://github.com/valeriejoplin/4013-Final-Project">GitHub Repo</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    
     <style>
         body {
             background-image: url('https://www.vestian.com/blog/wp-content/uploads/vestian-marathalli-6.jpg');
