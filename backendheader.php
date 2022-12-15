a4d50bbe65db16b48d992c4c471969e2ac8613f0
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-12-15 12:14:42 -0600

    Update backendheader.php

--- backendheader.php
+++ backendheader.php
@@ -42,7 +42,7 @@
                     <a class="nav-link" href="diagram.php">Database Diagram</a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="https://github.com/valeriejoplin/4013-Final-Project">GitHub Repo</a>
+                    <a class="nav-link" target=”_blank” href="https://github.com/valeriejoplin/4013-Final-Project">GitHub Repo</a>
                 </li>
             </ul>
         </div>
