fce761ad95ac8221d7f1e2fc14224a97f7f7dd0a
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-12-15 11:02:28 -0600

    updated diagram page

--- diagram.php
+++ diagram.php
@@ -1,1 +1,3 @@
-<img src="database diagram.png" alt="Diagram" width="500" height="600">
+<?php require_once("backendheader.php"); ?>
+
+<img src="database diagram.png" alt="Diagram" width="800" height="600">
