f7e6f1b1609aec806f670700e2ad1f573eb8c5a8
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-12-15 12:08:52 -0600

    Update login.php

--- login.php
+++ login.php
@@ -19,9 +19,7 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
         exit;
     } else {
         // Display an error message if the username or password is incorrect
-        echo "<h1>Invalid username or password. The correct username and password is admin and admin<h1> <a href="/backEndLogin.php">
-            <button class="btn btn-primary btn-lg">Click</button>
-        </a>";
+        echo "<h1>Invalid username or password. The correct username and password is admin and admin<h1> <a href="/backEndLogin.php"> <button class="btn btn-primary btn-lg">Click</button> </a>";
     }
 }
 
