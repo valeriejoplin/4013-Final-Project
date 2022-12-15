cad4762e601140b4a86768553edce88930ce8198
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-12-15 12:49:56 -0600

    Update diagram.php

--- diagram.php
+++ diagram.php
@@ -1,7 +1,2 @@
 <?php require_once("backendheader.php"); ?>
-<style>
-  .diagram{
-    left: 100px;
-  }
-  </style>
 <img src="database diagram.png" alt="Diagram" width="800" height="600" class"diagram">
