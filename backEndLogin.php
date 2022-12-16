29f426ba9c605ccea45a84fa376a6e0465b97dcd
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-12-15 21:09:49 -0600

    Update backEndLogin.php

--- backEndLogin.php
+++ backEndLogin.php
@@ -1,7 +1,9 @@
 <form method="POST" action="login.php">
-    <label for="username">Username:</label><br>
+    <label for="username">Username:</label>
+    <label for="username">admin</label><br>
     <input type="text" id="username" name="username"><br>
-    <label for="password">Password:</label><br>
+    <label for="password">Password:</label><
+    <label for="username">admin</label><br>
     <input type="password" id="password" name="password"><br><br>
     <input type="submit" value="Submit">
 </form>
